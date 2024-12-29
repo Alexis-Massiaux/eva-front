5fab42876d984b33b501585bb1db2f6a74cf504c
Alexis Massiaux <alexis.massiaux@worldline.com>
2024-12-29 14:50:16 +0100

    feat: create enum with columns and theirs display name for score tab

---
+++ src/app/models/score-tab-column.ts
@@ -0,0 +1,21 @@
+export enum ScoreTabColumn {
+  pseudo = "Pseudo",
+  seasonPass = "Season Pass",
+  level = "Level",
+  gameNumber = "Game Number",
+  wins = "Wins",
+  defeats = "Defeats",
+  ties = "Ties",
+  gameTime = "Game Time",
+  kills = "Kills",
+  deaths = "Deaths",
+  supports = "Supports",
+  percentKills = "Percent Kills",
+  killsForDeaths = "K/D",
+  distance = "Distance",
+  averageDistance = "Average Distance",
+  maxSeriesKills = "Max Series Kills",
+  maxDamage = "Max Damage",
+  totalDamage = "Total Damage"
+
+}
