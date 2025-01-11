216336082ed00f42eedadf3c7757bda07b93b044
Alexis Massiaux <alexis.massiaux@worldline.com>
2025-01-11 15:42:47 +0100

    feat: add eva header component from library with file: option
    possible to add component with npm link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ScoreTabComponent } from '@components/score-tab/score-tab.component';
+import { EvaHeaderComponent } from 'eva-library';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ScoreTabComponent],
+  imports: [RouterOutlet, ScoreTabComponent, EvaHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
